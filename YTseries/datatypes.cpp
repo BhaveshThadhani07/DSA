c514951b55ab8081fd04cc61c9802e68adc21a60
bhavesh-thadhani-github <bhaveshthadhani2@gmail.com>
2024-10-27 21:05:54 +0530

    New Datatypes: 1)Derived & 2)User Defined

--- YTseries/datatypes.cpp
+++ YTseries/datatypes.cpp
@@ -140,7 +140,7 @@ using namespace std;
 }*/
 
 // 03) ESCAPE SEQUENCE
-int main()
+/*int main()
 {
     // output may depend upon the compiler
     printf("My mobile number "
@@ -201,6 +201,190 @@ int main()
     // printf("%s", s2);
 
     return 0;
+}*/
+
+// 04) DERIVED DATA TYPES IN C++
+// 1. FUNCTIONS
+/*int max(int x, int y)
+{
+     if (x > y)
+          return x;
+     else
+          return y;
+}
+int main()
+{
+     int a = 10, b = 20;
+
+     int m = max(a, b);
+
+     cout << "Maximum is  "  << m;
+     return 0;
+}*/
+
+// 2. ARRAYS
+// integer array ‘arr’ and assigns values using indices. Then it prints all the array elements
+/*int main()
+{
+     int arr[5];
+     arr[0] = 5;
+     arr[2] = -10;
+
+     // this is same as arr[1] = 2
+     arr[3/2] = 2;
+
+     arr[3] = arr[0];
+
+     cout << arr[0] << " " << arr[1] << " " << arr[2] << " " << arr[3];
+
+     return 0;
+}*/
+
+// 3. POINTERS
+// Pointers are symbolic representation of addresses. They enable programs to simulate call-by-reference as well as to create and manipulate dynamic data structures.
+
+/*void geeks()
+{
+     int number = 5;
+
+     // declare pointer variable
+     int *ptr; // This declares a pointer to an integer
+
+     // datatype of ptr & var must be same
+     ptr = &number; // here the variable ptr(which is declared earlier) holds the memory address for the variable var which stores the value 20 in it
+
+     // assign the address of a variable to a pointer
+     cout << "Value of number: " << number << endl;               // Outputs 5
+     cout << "Address of number: " << &number << endl;            // Outputs the memory address of 'number'
+     cout << "Value of ptr (address of number): " << ptr << endl; // Same as &number
+     cout << "Value pointed to by ptr: " << *ptr << endl;         // Outputs 5 (value of 'number' via pointer)
+}
+int main()
+{
+     geeks();
+}*/
+
+// Why Use Pointers?
+// Direct Memory Access: You can modify values in memory directly.
+// Efficient Function Arguments: Pass large data (like arrays or structs) by address instead of copying them.
+// Dynamic Memory Management: Pointers allow dynamic allocation and deallocation of memory.
+
+// 4. REFERENCE
+// A reference is like an alias or another name for a variable, allowing us to work with the same data using different names.
+int main()
+{
+     int x = 10;
+
+     int& ref = x;  // We create a reference 'ref' that refers to 'x'.
+                     // Now 'ref' is another name for 'x', so any changes made to 'ref' affect 'x', and vice versa.
+
+     // We assign 20 to 'ref', which is a reference to 'x'.
+     //         This changes the value of 'x' to 20, since 'ref' and 'x' are essentially the same variable.
+     //value of x is now changed to 20
+     ref = 20;
+     cout << "x = " << x << endl;
+
+     //value of x is now changed to 30
+     x = 30;
+     cout << "ref = " << ref << endl;
+
+     return 0;
 }
 
+// This concept is helpful when we want to work with the same variable under different names, especially in functions, where passing by reference can avoid unnecessary copies.
 
+// ------------------------------------------------
+// 05) USER DEFINED DATA TYPES IN C++
+// 1. CLASS
+/*class Geeks{
+// access specifier
+public:
+     // *data members
+     string geekname;
+
+     // *member functions
+     void printname(){
+          cout << "Geekname is: " << geekname;
+     };
+
+};*/
+
+/*int main()
+{
+//Declare an object of class Geeks
+     Geeks obj1;
+
+//accessing data member
+     obj1.geekname="GeeksForGeeks";
+
+//accessing member function
+     obj1.printname();
+
+     return 0;
+}*/
+// Explaination: The above program defines a class named “Geeks” with a geekname attribute and a function printname() to print the geek’s name. In the main function, it creates an object named obj1, sets the geekname as “GeeksforGeeks”, and calls the printname() function to display it.
+
+// 2. STRUCTURE
+/*struct Point{
+     int x, y;
+};
+
+int main()
+{
+     // Create an array of structures
+    struct Point arr[10];
+
+    // Access array members
+    arr[0].x = 10;
+    arr[0].y = 20;
+
+    cout << arr[0].x << ", " << arr[0].y;
+     return 0;
+}*/
+
+// 3. UNION
+/*union test {
+     int x, y;
+};
+
+int main()
+{
+     union test t;
+
+     t.x = 2;
+
+     cout << "After making the value of x = 2, " << endl << "value of x: " << t.x << endl << "value of y: " << t.y << endl << endl;
+
+     t.y = 10;
+     cout << "When y = 10, " << endl << "value of x: " << t.x << endl << "value of y: " << t.y << endl << endl;
+
+     return 0;
+}*/
+// Explaination: Union named “test” with integer members x and y is defined, here x and y shares the same memory space. In the main function value of x is set to 2 and then printed. Later, it updates y to 10 and the value of x is also updated to 10, this shows the shared memory characteristic of unions.
+
+// 4. ENUMERATION
+/*enum week {mon, tue, wed, thur, fri, sat, sun};
+
+int main()
+{
+     enum week day;
+
+     day = wed;
+
+     cout << "Day: " << day;
+
+     return 0;
+}*/
+
+// 5. TYPEDEF
+// C++ allows you to define explicitly new data type names by using the keyword typedef. Using typedef does not create a new data class, rather it defines a name for an existing type. This can increase the portability(the ability of a program to be used across different types of machines; i.e., mini, mainframe, micro, etc; without many changes to the code)of a program as only the typedef statements would have to be changed. Using typedef one can also aid in self-documenting code by allowing descriptive names for the standard data types.
+
+/*typedef unsigned char BYTE;
+
+int main()
+{
+     BYTE b1, b2;
+     b1 = 'c';
+     cout << " " << b1;
+     return 0;
+}*/
